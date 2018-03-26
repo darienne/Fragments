001947b2f9a65cee7ae1f259c494106562d679bf
Darienne Miller <31522424+darienne@users.noreply.github.com>
2018-03-26 10:55:59 -0400

    asdf

---
+++ Fragments/app/src/main/java/com/example/dar/fragments/FragmentActivity.java
@@ -0,0 +1,23 @@
+package com.example.dar.fragments;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+/**
+ * Created by Dar on 3/25/2018.
+ */
+
+public class FragmentActivity extends Fragment {
+
+    public FragmentActivity() {
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.bottom_layout,
+                container, false);
+    }
+}
