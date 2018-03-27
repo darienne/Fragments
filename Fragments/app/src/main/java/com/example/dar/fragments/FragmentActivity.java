64a46e759c4b208cf6ecfa7fca6306ed6f357780
Darienne Miller <31522424+darienne@users.noreply.github.com>
2018-03-27 13:55:18 -0400

    added middle

--- Fragments/app/src/main/java/com/example/dar/fragments/FragmentActivity.java
+++ Fragments/app/src/main/java/com/example/dar/fragments/FragmentActivity.java
@@ -2,6 +2,8 @@ package com.example.dar.fragments;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,11 +15,14 @@ public class FragmentActivity extends Fragment {
 
     public FragmentActivity() {
     }
+
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.bottom_layout,
-                container, false);
+        return inflater.inflate( R.layout.bottom_layout,
+                container, false );
+
     }
+
 }
