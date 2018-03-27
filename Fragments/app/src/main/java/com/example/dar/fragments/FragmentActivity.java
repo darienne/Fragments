a570958f725b9f092f9ecb9acedfc94e101533fe
seanrickard <srickard@wvup.edu>
2018-03-27 15:46:21 -0400

    layout

--- Fragments/app/src/main/java/com/example/dar/fragments/FragmentActivity.java
+++ Fragments/app/src/main/java/com/example/dar/fragments/FragmentActivity.java
@@ -20,7 +20,7 @@ public class FragmentActivity extends Fragment {
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate( R.layout.bottom_layout,
+        return inflater.inflate( R.layout.fragment_player_prompt,
                 container, false );
 
     }
