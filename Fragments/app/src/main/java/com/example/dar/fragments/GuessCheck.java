2ebe2320b06eeb584e726f3c7db1ebaa45f4c282
Darienne Miller <31522424+darienne@users.noreply.github.com>
2018-03-28 00:11:49 -0400

    added GuessCheck

--- Fragments/app/src/main/java/com/example/dar/fragments/GuessCheck.java
+++ Fragments/app/src/main/java/com/example/dar/fragments/GuessCheck.java
@@ -1,18 +1,29 @@
 package com.example.dar.fragments;
 
 /**
- * Created by seanr on 3/27/2018.
+ GuessCheck is going to set a random number and have the user input a number and guesscheck the answer.
+ * If the user guesses it right then will return you got it.
+ * If user does not guess it right then will return to low, to high, or not even close.
+ * Created by Group 3-1 on 3/27/2018.
  */
 
 public class GuessCheck
 {
     int randomNumber;
 
+    /**
+     * GuessCheck contains an integer randomNumber which will set RandomNumber.
+     * @param randomNumber
+     */
     public GuessCheck(int randomNumber)
     {
         setRandomNumber(randomNumber);
     }
 
+    /**
+     * setRandomNumber contains an integer number which will have a param number.
+     * @param number
+     */
     public void setRandomNumber(int number)
     {
         randomNumber = number;
