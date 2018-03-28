53fbed11d8f6272136ac18d536dca0b3173f6dd9
Darienne Miller <31522424+darienne@users.noreply.github.com>
2018-03-28 00:38:11 -0400

    added commenting

--- Fragments/app/src/main/java/com/example/dar/fragments/FragmentActivity.java
+++ Fragments/app/src/main/java/com/example/dar/fragments/FragmentActivity.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 /**
  * FragmentActivity extends Fragment which then gets and return the view of the fragment_player_prompt.
  * Created by Dar on 3/25/2018.
  */
-
 public class FragmentActivity extends Fragment {
 
     public FragmentActivity() {
--- Fragments/app/src/main/java/com/example/dar/fragments/GameControlFrag.java
+++ Fragments/app/src/main/java/com/example/dar/fragments/GameControlFrag.java
@@ -4,7 +4,9 @@ import android.app.Fragment;
 import android.util.Log;
 
 /**
- * Created by seanr on 3/27/2018.
+ * GameControlFrag extends Fragment has an integer number and setNumber will set the number.
+ * getNumber will return the number.
+ * Created by Group 3-1 on 3/27/2018.
  */
 
 public class GameControlFrag extends Fragment
@@ -12,11 +14,19 @@ public class GameControlFrag extends Fragment
     int number;
     public GameControlFrag(){}
 
+    /**
+     * setNumber will take the integer number and set the number.
+     * @param number
+     */
     public void setNumber(int number)
     {
         this.number = number;
     }
 
+    /**
+     * getNumber will return the number.
+     * @return
+     */
     public int getNumber()
     {
         Log.i("gameControlGetNumber", " " + number);
